119cdb1891bf3e3be0ed93f72e4db2fe04d48748
xbroindia <shahhamdan451@gmail.com>
2026-02-21 12:30:18 +0530

    updated

---
+++ assignment1.py
@@ -0,0 +1,15 @@
+num1 = input("Enter the First number:")
+num2 = input("Enter the Second number:")
+
+Num1 = int(num1)
+Num2 = int(num2)
+
+ADD = Num1 + Num2
+SUB = Num1 - Num2
+MUL = Num1 * Num2
+DIV = Num1 / Num2
+
+print(f"Addtion: {ADD}")
+print(f"Substraction: {SUB}")
+print(f"Muliplication: {MUL}")
+print(f"Division: {DIV}")
